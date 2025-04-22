4eab246b8aafb6401d0a5527e47efc8d7f7721b6
pAplakidis <aplakidisp@gmail.com>
2025-04-22 11:19:02 +0300

    feat: CILv3D model + train-eval skeleton

--- CILv3D/model.py
+++ CILv3D/model.py
@@ -1,54 +1,65 @@
 #!/usr/bin/env python3
 import torch
 import torch.nn as nn
+from dataclasses import dataclass
 
 from config import *
 from models.uniformer_video.uniformer import uniformer_small
 
 
+@dataclass
+class CILv3DConfig:
+  sequence_size: int = SEQUENCE_SIZE
+  state_size: int = 7
+  command_size: int = 6
+  transformer_heads: int = 8
+  transformer_layers: int = 6
+  filters_1d: int = 32
+  filters_3d: int = 512
+  embedding_size: int = 512
+  freeze_backbone: bool = True
+
+
 class CILv3D(nn.Module):
-  def __init__(
-      self,
-      sequence_size: int,
-      state_size: int,
-      command_size: int,
-      filters_1d: int,
-      embedding_size: int = 512,  # TODO: make dynamic (backbone_out.shape[-1])
-      freeze_backbone: bool = True
-    ):
+  def __init__(self, cfg: CILv3DConfig = CILv3DConfig()):
     super(CILv3D, self).__init__()
 
-    self.sequence_size = sequence_size
-    self.state_size = state_size
-    self.command_size = command_size
-    self.filters_1d = filters_1d
-    self.embedding_size = embedding_size
+    self.sequence_size = cfg.sequence_size
+    self.state_size = cfg.state_size
+    self.command_size = cfg.command_size
+    self.filters_1d = cfg.filters_1d
+    self.filters_3d = cfg.filters_3d
+    self.embedding_size = cfg.embedding_size
+    self.transformer_heads = cfg.transformer_heads
+    self.transformer_layers = cfg.transformer_layers
+    self.freeze_backbone = cfg.freeze_backbone
 
     uniformer_state_dict = torch.load('models/state_dicts/uniformer_small_k400_16x8.pth', map_location='cpu')
     self.uniformer = uniformer_small()
     self.uniformer.load_state_dict(uniformer_state_dict)
     self.uniformer.head = nn.Identity()
 
-    if freeze_backbone:
+    if self.freeze_backbone:
       for param in self.uniformer.parameters():
         param.requires_grad = False
 
-    # TODO: for CILv3D:
-    # tf.keras.Sequential(
-    #         layers=[
-    #             tf.keras.layers.Conv1D(filters=self._filters_1d, kernel_size=self._1d_kernel_size, activation=self._config.activation),
-    #             tf.keras.layers.BatchNormalization(),
-    #             tf.keras.layers.Flatten(),
-    #             tf.keras.layers.Dense(units=embeddings_size)
-    #         ]
-    #   )
-    self.state_embedding = nn.Linear(state_size, embedding_size)
-    self.command_embedding = nn.Linear(command_size, embedding_size)
+    self.state_embedding = nn.Sequential(
+      nn.Conv1d(in_channels=self.sequence_size, out_channels=self.filters_1d, kernel_size=1),
+      nn.BatchNorm1d(self.filters_1d),
+      nn.Flatten(),
+      nn.Linear(self.filters_1d * self.state_size, self.embedding_size),
+    )
+    self.command_embedding = nn.Sequential(
+      nn.Conv1d(in_channels=self.sequence_size, out_channels=self.filters_1d, kernel_size=1),
+      nn.BatchNorm1d(self.filters_1d),
+      nn.Flatten(),
+      nn.Linear(self.filters_1d * self.command_size, self.embedding_size),
+    )
 
-    encoder_layer = nn.TransformerEncoderLayer(d_model=embedding_size, nhead=TRANSFORMER_HEADS)
-    self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=TRANSFORMER_LAYERS)
+    encoder_layer = nn.TransformerEncoderLayer(d_model=self.embedding_size, nhead=self.transformer_heads)
+    self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=self.transformer_layers)
 
-    self.linear = nn.Linear(embedding_size, 2)
+    self.linear = nn.Linear(self.embedding_size, 2)
 
   def forward(self, x, states, commands):
     """
@@ -67,17 +78,18 @@ class CILv3D(nn.Module):
     # layerout = layerout[0].detach().cpu().permute(1, 2, 3, 0)
 
     z = vision_embeddings + control_embedding
-    out = self.linear(self.transformer_encoder(z)[:, -1])
+    out = self.linear(self.transformer_encoder(z))
     return out
 
 
 if __name__ == "__main__":
-  model = CILv3D(SEQUENCE_SIZE, STATE_SIZE, COMMAND_SIZE, FILTERS_1D)
+  model = CILv3D()
   print(model)
 
-  # get predictions, last convolution output and the weights of the prediction layer
-  vid = torch.randn(1, 3, SEQUENCE_SIZE, IMAGE_SIZE[0], IMAGE_SIZE[1])  # B, C, T, H, W
-  states = torch.randn(1, SEQUENCE_SIZE, STATE_SIZE) # B, T, S
-  commands = torch.randn(1, SEQUENCE_SIZE, COMMAND_SIZE) # B, T, C
+  bs = 2
+  # FIXME: we have 3 images per time step
+  vid = torch.randn(bs, 3, SEQUENCE_SIZE, IMAGE_SIZE[0], IMAGE_SIZE[1])  # B, C, T, H, W
+  states = torch.randn(bs, SEQUENCE_SIZE, 7) # B, T, S
+  commands = torch.randn(bs, SEQUENCE_SIZE, 6) # B, T, C
   out = model(vid, states, commands)
   print(out.shape)
--- CILv3D/trainer.py
+++ CILv3D/trainer.py
@@ -1,8 +1,11 @@
 import os
+import numpy as np
+from tqdm import tqdm
 from datetime import datetime
 from typing import Optional
 
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
@@ -35,20 +38,87 @@ class Trainer:
       today = str(datetime.now()).replace(" ", "_")
       auto_name = "-".join([model_path.split('/')[-1].split('.')[0], today, f"lr_{LR}", f"bs_{BATCH_SIZE}"])
       writer_path = "runs/" + auto_name
-    print("[+] Tensorboard output path:", writer_path)
+    print("[*] Tensorboard output path:", writer_path)
     self.writer = SummaryWriter(writer_path)
 
   def save_checkpoint(self, min_loss):
-    pass
+    chpt_path = self.model_path.split(".")[0] + f"_best.pt"
+    torch.save(self.model.state_dict(), chpt_path)
+    print(f"[+] Checkpoint saved at {chpt_path}. New min eval loss {min_loss}")
 
-  def train_step(self):
-    pass
+  def train_step(self, t, i_batch, sample_batched, loss_func, optim, epoch_losses):
+    X = sample_batched[0]["x"].to(self.device)
+    STATES = sample_batched[0]["states"].to(self.device)
+    COMMANDS = sample_batched[0]["commands"].to(self.device)
+    Y = sample_batched[1].to(self.device)
+    out = self.model(X, STATES, COMMANDS)
+
+    optim.zero_grad()
+    loss = loss_func(out, Y).mean()
+    loss.backward()
+    optim.step()
+
+    self.writer.add_scalar("running loss", loss.item(), i_batch)
+    epoch_losses.append(loss.item())
+    t.set_description(f"[train] Batch loss: {loss.item():.4f}")
 
   def train(self):
-    pass
+    loss_func = nn.L1Loss()
+    optim = torch.optim.AdamW(self.model.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
+    losses, vlosses = [], []
 
-  def eval_step(self):
-    pass
+    try:
+      print("[*] Training...")
+      min_epoch_vloss = float("inf")
+      for epoch in range(EPOCHS):
+        self.model.train()
+        print(f"\n[=>] Epoch {epoch+1}/{EPOCHS}")
+        epoch_losses, epoch_vlosses = [], []
 
-  def eval(self):
-    pass
+        for i_batch, sample_batched in enumerate((t := tqdm(self.train_loader))):
+          self.train_step(t, i_batch, sample_batched, loss_func, optim, epoch_losses)
+
+        avg_epoch_loss = np.array(epoch_losses).mean()
+        losses.append(avg_epoch_loss)
+        self.writer.add_scalar("epoch training loss", avg_epoch_loss, epoch)
+        print("[->] Epoch average training loss: %.4f"%(avg_epoch_loss))
+
+        avg_epoch_vloss = None
+        if self.eval_epoch:
+          self.eval(loss_func, epoch_vlosses)
+          avg_epoch_vloss = np.array(epoch_vlosses).mean()
+          vlosses.append(avg_epoch_vloss)
+          self.writer.add_scalar("epoch validation loss", avg_epoch_vloss, epoch)
+          print("[->] Epoch average validation loss: %.4f"%(avg_epoch_vloss))
+
+        if self.save_checkpoints and avg_epoch_vloss is not None and avg_epoch_vloss < min_epoch_vloss:
+          min_epoch_vloss = avg_epoch_vloss
+          self.save_checkpoint(min_epoch_vloss)
+    except KeyboardInterrupt:
+      print("[*] Training interrupted. Saving model...")
+
+    print("[+] Training done")
+    torch.save(self.model.state_dict(), self.model_path)
+    print(f"[+] Model saved at {self.model_path}")
+
+  def eval_step(self, t, i_batch, sample_batched, loss_func, epoch_vlosses):
+    X = sample_batched[0]["x"].to(self.device)
+    STATES = sample_batched[0]["states"].to(self.device)
+    COMMANDS = sample_batched[0]["commands"].to(self.device)
+    Y = sample_batched[1].to(self.device)
+    out = self.model(X, STATES, COMMANDS)
+
+    loss = loss_func(out, Y).mean()
+    self.writer.add_scalar("running loss", loss.item(), i_batch)
+    epoch_vlosses.append(loss.item())
+    t.set_description(f"[val] Batch loss: {loss.item():.4f}")
+
+  def eval(self, loss_func, epoch_vlosses):
+    with torch.no_grad():
+      self.model.eval()
+      print("[*] Evaluating...")
+      try:
+        for i_batch, sample_batched in enumerate((t := tqdm(self.val_loader))):
+          self.eval_step(t, i_batch, sample_batched, loss_func, epoch_vlosses)
+      except KeyboardInterrupt:
+        print("[*] Evaluation interrupted.")
