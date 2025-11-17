f3e3e3d7fac685255bea275b7a12b10bbc44f45c
pAplakidis <aplakidisp@gmail.com>
2025-11-17 16:52:06 +0200

    image distributions

---
+++ CILv3D/CityscapesWaymoDataset.py
@@ -0,0 +1,76 @@
+import os
+import random
+import numpy as np
+import torch
+from typing import Tuple
+from PIL import Image
+from torchvision import transforms as TR
+
+
+class CityscapesWaymoDataset(torch.utils.data.Dataset):
+    def __init__(self, opt, for_metrics):
+        opt.load_size = 512
+        opt.crop_size = 512
+        opt.label_nc = 35
+        opt.contain_dontcare_label = True
+        opt.semantic_nc = 36
+        opt.cache_filelist_read = False
+        opt.cache_filelist_write = False
+        opt.aspect_ratio = 2.0
+
+        self.opt = opt
+        self.for_metrics = for_metrics
+        self.images, self.labels, self.paths = self.list_images()
+
+    def __len__(self,):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = Image.open(os.path.join(self.paths[0], self.images[idx])).convert('RGB')
+        label = Image.open(os.path.join(self.paths[1], self.labels[idx]))
+        image, label = self.transforms(image, label)
+        label = label * 255
+        return {"image": image, "label": label, "name": self.images[idx]}
+
+    def list_images(self):
+        mode = "val" if self.opt.phase == "test" or self.for_metrics else "train"
+        images = []
+        path_img = os.path.join(self.opt.dataroot, "leftImg8bit", mode)
+        for city_folder in sorted(os.listdir(path_img)):
+            cur_folder = os.path.join(path_img, city_folder)
+            for item in sorted(os.listdir(cur_folder)):
+                images.append(os.path.join(city_folder, item))
+        labels = []
+        path_lab = os.path.join(self.opt.dataroot, "gtFine", mode)
+        for city_folder in sorted(os.listdir(path_lab)):
+            cur_folder = os.path.join(path_lab, city_folder)
+            for item in sorted(os.listdir(cur_folder)):
+                if item.find("labelIds") != -1:
+                    labels.append(os.path.join(city_folder, item))
+        assert len(images)  == len(labels), "different len of images and labels %s - %s" % (len(images), len(labels))
+        for i in range(len(images)):
+            assert images[i].replace("_leftImg8bit.png", "") == labels[i].replace("_gtFine_labelIds.png", ""),\
+                '%s and %s are not matching' % (images[i], labels[i])
+        return images, labels, (path_img, path_lab)
+
+    def transforms(self, image: np.ndarray, label: np.ndarray) -> Tuple[torch.Tensor, torch.Tensor]:
+        assert image.size == label.size
+
+        # Resize image & label
+        new_width, new_height = (int(self.opt.load_size / self.opt.aspect_ratio), self.opt.load_size)
+        image = TR.functional.resize(image, (new_width, new_height), Image.BICUBIC)
+        label = TR.functional.resize(label, (new_width, new_height), Image.NEAREST)
+
+        # Flip image & label horizontally during training
+        if not (self.opt.phase == 'test' or self.opt.no_flip or self.for_metrics):
+            if random.random() < 0.5:
+                image = TR.functional.hflip(image)
+                label = TR.functional.hflip(label)
+
+        # Convert inputs to tensors
+        image = TR.functional.to_tensor(image)
+        label = TR.functional.to_tensor(label)
+
+        # Normalize the images
+        image = TR.functional.normalize(image, (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        return image, label
